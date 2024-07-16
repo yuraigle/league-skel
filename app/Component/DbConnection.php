4cf7e65b08ed7f98f18a259b42791070bda7f600
yuraigle <yuraigle@gmail.com>
2024-07-16 20:43:51 +0800

    ...

--- app/Component/DbConnection.php
+++ app/Component/DbConnection.php
@@ -2,9 +2,18 @@
 
 namespace App\Component;
 
-class DbConnection
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+
+class DbConnection implements LoggerAwareInterface
 {
     private static \mysqli | null $conn = null;
+    private LoggerInterface $logger;
+
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
+    }
 
     public function getConn(): \mysqli
     {
@@ -12,12 +21,17 @@ class DbConnection
             return self::$conn;
         }
 
-        self::$conn = new \mysqli(
-            $_ENV['DB_HOST'],
-            $_ENV['DB_USER'],
-            $_ENV['DB_PASS'],
-            $_ENV['DB_NAME']
-        );
+        try {
+            self::$conn = new \mysqli(
+                $_ENV['DB_HOST'],
+                $_ENV['DB_USER'],
+                $_ENV['DB_PASS'],
+                $_ENV['DB_NAME']
+            );
+        } catch (\Throwable $e) {
+            $this->logger->critical("DB DOWN: " . $e->getMessage());
+            throw new \RuntimeException($e->getMessage());
+        }
 
         return self::$conn;
     }
