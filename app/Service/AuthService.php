945d2b6c81ec47e97a3d229b8eca0a28f1b6a49c
yuraigle <yuraigle@gmail.com>
2025-02-02 16:23:47 +0800

    pass hashed

--- app/Service/AuthService.php
+++ app/Service/AuthService.php
@@ -39,9 +39,9 @@ class AuthService
      */
     public function authenticate(string $user, string $pass): array
     {
-        $sql = "select id, username from `users` where username = ? and password = ?";
+        $sql = "select id, username, password from `users` where username = ?";
         $stmt = $this->db->getConn()->prepare($sql);
-        $stmt->bind_param("ss", $user, $pass);
+        $stmt->bind_param("s", $user);
         $stmt->execute();
 
         if ($stmt->error) {
@@ -50,9 +50,54 @@ class AuthService
 
         $result = $stmt->get_result();
         if ($row = $result->fetch_assoc()) {
-            return $row;
+            if (password_verify($pass, $row['password'])) {
+                return ['id' => $row['id'], 'username' => $row['username']];
+            }
         }
 
         throw new Exception("Wrong credentials");
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function createTestUser(string $username, string $password): int
+    {
+        if ($this->isUsernameTaken($username)) {
+            throw new Exception("Username already taken.");
+        }
+
+        $passHash = password_hash($password, PASSWORD_DEFAULT);
+
+        $sql = "insert into `users` (username, password) values (?, ?)";
+        $stmt = $this->db->getConn()->prepare($sql);
+        $stmt->bind_param("ss", $username, $passHash);
+        $stmt->execute();
+
+        return $stmt->insert_id;
+    }
+
+    private function isUsernameTaken(string $username): bool
+    {
+        $sql = "select id from `users` where username = ?";
+        $stmt = $this->db->getConn()->prepare($sql);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+
+        return $stmt->get_result()->num_rows > 0;
+    }
+
+    public function isUsersTableEmpty(): bool
+    {
+        $sql = "select count(*) as cnt from `users`";
+        $stmt = $this->db->getConn()->prepare($sql);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        if ($row = $result->fetch_assoc()) {
+            return $row['cnt'] === 0;
+        }
+
+        return false;
     }
 }
